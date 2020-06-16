3a99fc3b7efffa4978f052612df4f8dd528870e9
NaynaV <nainavaghasiya9@gmail.com>
2020-06-16 18:09:44 -0400

    necessary functions added for animation

--- Panda Jumper/GameScene.swift
+++ Panda Jumper/GameScene.swift
@@ -10,80 +10,278 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
+    let mario = SKSpriteNode(imageNamed: "mario1")
+     var lastUpdateTime: TimeInterval = 0
+     var dt: TimeInterval = 0
+     let marioMovePointsPerSec: CGFloat = 480.0
+     var velocity = CGPoint.zero
+     let playableRect: CGRect
+     var lastTouchLocation: CGPoint?
+     let marioMove: SKAction
+     let marioAnimation: SKAction
+     let jumpSound: SKAction = SKAction.playSoundFileNamed(
+       "jumpSound.wav", waitForCompletion: false)
+     let enemyCollisionSound: SKAction = SKAction.playSoundFileNamed(
+       "loselifeSound.wav", waitForCompletion: false)
+     var invincible = false
+
+     var lives = 7
+     var gameOver = false
+     let cameraNode = SKCameraNode()
+     let cameraMovePointsPerSec: CGFloat = 200.0
+
+     let livesLabel = SKLabelNode(fontNamed: "Chalkduster")
+
+       
+     override init(size: CGSize) {
+       let maxAspectRatio:CGFloat = 16.0/9.0
+       let playableHeight = size.width / maxAspectRatio
+       let playableMargin = (size.height-playableHeight)/2.0
+       playableRect = CGRect(x: 0, y: playableMargin,
+                             width: size.width,
+                             height: playableHeight)
+       
+       // 1
+       var textures:[SKTexture] = []
+       // 2
+       for i in 1...12 {
+         textures.append(SKTexture(imageNamed: "mario\(i)"))
+       }
+       // 3
+       textures.append(textures[2])
+       textures.append(textures[1])
+
+       // 4
+       marioAnimation = SKAction.animate(with: textures,
+         timePerFrame: 0.1)
+       marioMove = SKAction.moveBy(x: 0 + mario.size.width, y: 0, duration: 1.5)
+     
+       super.init(size: size)
+     }
+
+     required init(coder aDecoder: NSCoder) {
+       fatalError("init(coder:) has not been implemented")
+     }
+     
+     var cameraRect : CGRect {
+       let x = cameraNode.position.x - size.width/2
+           + (size.width - playableRect.width)/2
+       let y = cameraNode.position.y - size.height/2
+           + (size.height - playableRect.height)/2
+       return CGRect(
+         x: x,
+         y: y,
+         width: playableRect.width,
+         height: playableRect.height)
+     }
+     func debugDrawPlayableArea() {
+       let shape = SKShapeNode()
+       let path = CGMutablePath()
+       path.addRect(playableRect)
+       shape.path = path
+       shape.strokeColor = SKColor.red
+       shape.lineWidth = 4.0
+       addChild(shape)
+     }
+       func moveCamera() {
+          let backgroundVelocity =
+            CGPoint(x: cameraMovePointsPerSec, y: 0)
+          let amountToMove = backgroundVelocity * CGFloat(dt)
+          cameraNode.position += amountToMove
+          
+          enumerateChildNodes(withName: "bakground") { node, _ in
+            let background = node as! SKSpriteNode
+            if background.position.x + background.size.width <
+                self.cameraRect.origin.x {
+              background.position = CGPoint(
+                x: background.position.x + background.size.width*2,
+                y: background.position.y)
+            }
+          }
+          
+        }
+       func backgroundNode() -> SKSpriteNode {
+         // 1
+         let backgroundNode = SKSpriteNode()
+         backgroundNode.anchorPoint = CGPoint.zero
+         backgroundNode.name = "bakground"
+           backgroundNode.zPosition = -1
+
+         // 2
+         let background1 = SKSpriteNode(imageNamed: "bakground")
+         background1.anchorPoint = CGPoint.zero
+         background1.position = CGPoint(x: 0, y: 0)
+         backgroundNode.addChild(background1)
+         
+         // 3
+         let background2 = SKSpriteNode(imageNamed: "bakground")
+         background2.anchorPoint = CGPoint.zero
+         background2.position =
+           CGPoint(x: background1.size.width, y: 0)
+         backgroundNode.addChild(background2)
+
+         // 4
+         backgroundNode.size = CGSize(
+           width: background1.size.width + background2.size.width,
+           height: background1.size.height)
+         return backgroundNode
+       }
+     func spawnEnemy() {
+       let enemy = SKSpriteNode(imageNamed: "Enemy")
+       enemy.position = CGPoint(
+         x: cameraRect.maxX + enemy.size.width/2,
+         y: cameraRect.minY + 80)
+       enemy.zPosition = 50
+       enemy.name = "Enemy"
+       enemy.setScale(0.7)
+       addChild(enemy)
+       
+       let actionMove =
+         SKAction.moveBy(x: -(size.width + enemy.size.width), y: 0, duration: 4.0)
+       let actionRemove = SKAction.removeFromParent()
+       enemy.run(SKAction.sequence([actionMove, actionRemove]))
+     }
+       func move(sprite: SKSpriteNode, velocity: CGPoint) {
+         let amountToMove = CGPoint(x: velocity.x * CGFloat(dt),
+                                    y: velocity.y * CGFloat(dt))
+         sprite.position += amountToMove
+       }
+     override func didMove(to view: SKView) {
+
+       playBackgroundMusic(filename: "BgSound.wav")
+     
+       for i in 0...1 {
+         let background = backgroundNode()
+         background.anchorPoint = CGPoint.zero
+         background.position =
+           CGPoint(x: CGFloat(i)*background.size.width, y: 0)
+         background.name = "bakground"
+         background.zPosition = -1
+         addChild(background)
+       }
+       
+       mario.position = CGPoint(x: 400, y: 300)
+       mario.zPosition = 100
+       addChild(mario)
+       mario.run(SKAction.repeatForever(marioAnimation))
+       mario.run(SKAction.repeatForever(marioMove))
+       
+       run(SKAction.repeatForever(
+         SKAction.sequence([SKAction.run() { [weak self] in
+                         self?.spawnEnemy()
+                       },
+                       SKAction.wait(forDuration: 2.0)])))
+
+     
+       
+       // debugDrawPlayableArea()
+       
+       addChild(cameraNode)
+       camera = cameraNode
+       cameraNode.position = CGPoint(x: size.width/2, y: size.height/2)
+       
+       livesLabel.text = "Lives: X"
+       livesLabel.fontColor = SKColor.black
+       livesLabel.fontSize = 100
+       livesLabel.zPosition = 150
+       livesLabel.horizontalAlignmentMode = .left
+       livesLabel.verticalAlignmentMode = .bottom
+       livesLabel.position = CGPoint(
+           x: -playableRect.size.width/2 + CGFloat(20),
+           y: -playableRect.size.height/2 + CGFloat(20))
+       cameraNode.addChild(livesLabel)
+       
+       
+       
+     }
+      func marioHit(enemy: SKSpriteNode) {
+         invincible = true
+         let blinkTimes = 10.0
+         let duration = 3.0
+         let blinkAction = SKAction.customAction(withDuration: duration) { node, elapsedTime in
+           let slice = duration / blinkTimes
+           let remainder = Double(elapsedTime).truncatingRemainder(
+             dividingBy: slice)
+           node.isHidden = remainder > slice / 2
+         }
+         let setHidden = SKAction.run() { [weak self] in
+           self?.mario.isHidden = false
+           self?.invincible = false
+         }
+         mario.run(SKAction.sequence([blinkAction, setHidden]))
+         
+         run(enemyCollisionSound)
+         
+       
+         lives -= 1
+       
+       }
+       func checkCollisions() {
+
+         if invincible {
+           return
+         }
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+         var hitEnemies: [SKSpriteNode] = []
+         enumerateChildNodes(withName: "Enemy") { node, _ in
+           let enemy = node as! SKSpriteNode
+           if node.frame.insetBy(dx: 20, dy: 20).intersects(
+             self.mario.frame) {
+             hitEnemies.append(enemy)
+           }
+         }
+         for enemy in hitEnemies {
+           marioHit(enemy: enemy)
+         }
+       }
+     func sceneTouched(touchLocation:CGPoint) {
+       let actionJump : SKAction
+       actionJump = SKAction.moveBy(x: 0, y: 350, duration: 0.7)
+       let jumpSequence = SKAction.sequence([actionJump, actionJump.reversed()])
+       mario.run(jumpSequence)
+       
+       }
+       override func touchesBegan(_ touches: Set<UITouch>,
+            with event: UIEvent?) {
+          guard let touch = touches.first else {
+            return
+          }
+          let touchLocation = touch.location(in: self)
+          sceneTouched(touchLocation: touchLocation)
         }
+      
+     override func update(_ currentTime: TimeInterval) {
+     
+       if lastUpdateTime > 0 {
+         dt = currentTime - lastUpdateTime
+       } else {
+         dt = 0
+       }
+       lastUpdateTime = currentTime
+     
+       
+        move(sprite: mario, velocity: velocity)
+       moveCamera()
+       livesLabel.text = "Lives: \(lives)"
+       checkCollisions()
+     
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
+       if lives <= 0 && !gameOver {
+         gameOver = true
+         print("You lose!")
+         backgroundMusicPlayer.stop()
+         
+         // 1
+       
+       
+     }
+     
+       }
+     
     
+     
     
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
+
+       
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
 }
